825a0bdaa2426ad5dcbcaac26e4b6698e45ed9df
K <codehaag@gmail.com>
2018-11-13 22:19:07 -0700

    Added component folder and created navbar.js. completed navbar.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
       Working
       </div>
     );
