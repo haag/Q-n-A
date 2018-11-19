360287e3de7d95a5c6ace774a3a85eb5dc714409
K <codehaag@gmail.com>
2018-11-19 14:17:46 -0700

    More refactoring to validate session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import NavBar from './components/Navbar'
 import Questions from './components/Questions'
 import Question from './components/Question'
-import {Route} from 'react-router-dom'
+import {Route, withRouter} from 'react-router-dom'
 import Callback from './components/Callback';
 import NewQuestion from './components/NewQuestion'
 import SecuredRoute from './components/SecuredRoute'
 import auth0Client from './Auth';
 
 
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      checkingSession: true
+    }
+  }
   async componentDidMount() {
-    if(this.props.location.pathname === './callback') return
+    if(this.props.location.pathname === './callback') {
+      this.setState({checkingSession: false})
+      return
+    }
     try {
       await auth0Client.silentAuth()
       this.forceUpdate()
@@ -19,8 +29,9 @@ class App extends Component {
       if(err.error === 'login_required') return
       console.log(err.error)
     }
+    this.setState({checkingSession:false})
   }
-  
+
   render() {
     return (
       <div>
@@ -28,10 +39,13 @@ class App extends Component {
         <Route exact path='/' component={Questions} />
         <Route exact path='/question/:questionId' component={Question} />
         <Route exact path='/callback' component={Callback} />
-        <SecuredRoute path='/new-question' component={NewQuestion} />
+        <SecuredRoute path='/new-question' 
+          component={NewQuestion} 
+          checkingSession={this.state.checkingSession}
+          />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
